8e9c176e3180659b43f10dc395a7999f9f9288d1
vinaykulk621 <kulkarnivinay621@gmail.com>
2026-03-05 19:28:00 +0530

    feat: we see errors in line

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -55,5 +55,12 @@ vim.keymap.set("v", "<leader>/", "gc", { remap = true })
 vim.keymap.set("i", "<leader>/", "gc", { remap = true })
 
 
+--switching windows/tabs
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 
-
+--errors
+vim.diagnostic.config({ virtual_text = true })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
